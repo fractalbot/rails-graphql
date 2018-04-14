7aa0ccae42b14e38728690e2ef5eedd5d1dbc298
Gleydson <tavares.gleydson@gmail.com>
2018-04-14 09:08:24 -0300

    Adicionado validations no model user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -11,11 +11,16 @@ Types::UserType = GraphQL::ObjectType.define do
   name "UserType"
 
   field :id, types.ID
-  field :name, !types.String
+  field :name, types.String
   field :posts, types[Types::PostType]
   field :quant_posts, types.Int, "The quantity of posts" do
-    resolve ->(obj, _, _) {
+    resolve -> (obj, _, _) {
       obj.posts.count
+    }
+  end
+  field :errors, types[types.String], "Reasons this object could not be saved" do
+    resolve ->(obj,_,_){
+      obj.errors.to_a
     }
   end
 end
