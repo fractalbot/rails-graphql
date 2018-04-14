b43a47fb3e1ca45bea68decb7f5703e42c8489b1
Gleydson <tavares.gleydson@gmail.com>
2018-04-14 12:32:05 -0300

    removido cadastro de sessions

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -17,7 +17,7 @@ class GraphqlController < ApplicationController
 
   # gets current user from token stored in session
   def current_user
-    return nil
+    return unless session[:token]
 
     crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base.byteslice(0..31))
     token = crypt.decrypt_and_verify session[:token]
--- app/models/session.rb
+++
@@ -1,9 +0,0 @@
-class Session < ApplicationRecord
-  belongs_to :user
-
-  before_create do
-    self.key = SecureRandom.hex(50)
-  end
-
-  serialize :content
-end
