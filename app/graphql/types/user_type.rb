ee1e29630cdb553233a351f45180b40d746a3b8b
Gleydson <tavares.gleydson@gmail.com>
2018-04-14 10:15:19 -0300

    adicionado novos atributos em user_type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -12,6 +12,8 @@ Types::UserType = GraphQL::ObjectType.define do
 
   field :id, types.ID
   field :name, types.String
+  field :email, types.String
+  field :is_admin, types.Boolean
   field :posts, types[Types::PostType]
   field :quant_posts, types.Int, "The quantity of posts" do
     resolve -> (obj, _, _) {
