457b990b9f49db15f6848678db9164c39d6171b3
Gleydson <tavares.gleydson@gmail.com>
2018-04-16 11:37:30 -0300

    Criado resolver para o post

---
+++ app/graphql/resolvers/create_post.rb
@@ -0,0 +1,16 @@
+class Resolvers::CreatePost < GraphQL::Function
+  argument :title, !types.String
+  argument :content, !types.String
+
+  type Types::PostType
+
+  def call(_obj, args, ctx)
+    Post.create!(
+      title: args[:title],
+      content: args[:content],
+      user_id: ctx[:current_user]&.id
+    )
+  rescue ActiveRecord::RecordInvalid => e
+    GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+  end
+end
