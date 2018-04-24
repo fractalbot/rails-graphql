52ac09c6cc9307d769db80167f87dd015a3b5286
Gleydson <tavares.gleydson@gmail.com>
2018-04-24 16:26:30 -0300

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 user = User.create(name: 'Gleydson Tavares', email: 'teste@email.com.br', password:"12345", password_confirmation:"12345")
-10000.times do
+100.times do
   post = Post.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph)
   10.times do
     post.comments << Comment.create(comment: Faker::Lorem.paragraph)
